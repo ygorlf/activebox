033e35455aefee14e1bc34b144bf70e5a1b113c7
Ygor Fonseca <ygor.luiz.fonseca@gmail.com>
2015-10-25 23:47:53 -0200

    Add Fixed Navigation Effect

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
-/* Mobile Navigation */
-$(".mobile-nav").click(function() {
-	$("#header ul").toggleClass("navigation");
-	$(this).css("z-index", "1");
-	$(".mobile-nav i").toggleClass("fa-align-justify").addClass("fa-times");
-});
-
 $(function() {
+	/* Mobile Navigation */
+	$(".mobile-nav").click(function() {
+		$("#header ul").toggleClass("navigation");
+		$(this).css("z-index", "1");
+		$(".mobile-nav i").toggleClass("fa-align-justify").addClass("fa-times");
+	});
+	/* Fixed Navigation */
+	$(window).bind("scroll", function() {
+		if ($(window).scrollTop() > 90) {
+			$("#header nav").addClass("nav-fixed");
+		} else{
+			$("#header nav").removeClass("nav-fixed");
+		}
+	})
+	/* Slider */
 	$(".banner").unslider({
 		speed: 800,
 		keys: true,
